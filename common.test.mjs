a34dc4d7ab3c8517cf78e7eb62937e19116b47b0
SuWebOnes <surafealaw@gmail.com>
2025-10-08 00:31:41 +0100

    est if All user IDs are numbers in common.test.mjs

--- common.test.mjs
+++ common.test.mjs
@@ -5,3 +5,9 @@ import test from "node:test";
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
 });
+
+// Test name: "All user IDs are numbers"
+test('All user IDs are numbers', () => {
+  const users = getUserIds();
+  users.forEach(id => assert.strictEqual(typeof id, 'number'));
+});
