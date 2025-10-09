c41a7b824b579298b22b06d24bbc76f043bb752e
SuWebOnes <surafealaw@gmail.com>
2025-10-10 00:58:39 +0100

    All user IDs are strings test.mjs

--- common.test.mjs
+++ common.test.mjs
@@ -5,3 +5,8 @@ import test from "node:test";
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
 });
+
+test('All user IDs are strings', () => {
+  const users = getUserIds();
+  users.forEach(id => assert.strictEqual(typeof id, 'string'));
+});
